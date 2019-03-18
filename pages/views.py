b00f934fe4bd504f6c413dcd2b6acfae79285f2a
Juan Carlos <juanc.vivasm@gmail.com>
2019-03-18 16:26:37 -0400

    Vistas CRUD con CBV: CreateView

--- pages/views.py
+++ pages/views.py
@@ -2,6 +2,9 @@
 # from django.utils import timezone
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
+# from django.urls import reverse
+from django.urls import reverse_lazy
 from .models import Page
 
 # Create your views here.
@@ -21,3 +24,12 @@ class PageDetailView(DetailView):
     #    context = super().get_context_data(**kwargs)
     #    context['now'] = timezone.now()
     #    return context
+
+class PageCreate(CreateView):
+    model = Page
+    fields = ['title', 'content', 'order']
+
+    success_url = reverse_lazy('pages:pages')
+
+    #def get_success_url(self):
+    #    return reverse('pages:pages')
