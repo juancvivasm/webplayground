2e55def425bc984eb68b116a87195de24d0f5925
Juan Carlos <juanc.vivasm@gmail.com>
2019-03-18 15:19:32 -0400

    Para la App Pages (ListView - DetailView)

--- pages/views.py
+++ pages/views.py
@@ -1,11 +1,23 @@
-from django.shortcuts import render, get_object_or_404, get_list_or_404
+# from django.shortcuts import render, get_object_or_404, get_list_or_404
+# from django.utils import timezone
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from .models import Page
 
 # Create your views here.
-def pages(request):
-    pages = get_list_or_404(Page)
-    return render(request, 'pages/pages.html', {'pages':pages})
+class PageListView(ListView):
+    model = Page
+    #paginate_by = 100  # if pagination is desired
 
-def page(request, page_id, page_slug):
-    page = get_object_or_404(Page, id=page_id)
-    return render(request, 'pages/page.html', {'page':page})
+    #def get_context_data(self, **kwargs):
+    #    context = super().get_context_data(**kwargs)
+    #    context['now'] = timezone.now()
+    #    return context
+
+class PageDetailView(DetailView):
+    model = Page
+
+    #def get_context_data(self, **kwargs):
+    #    context = super().get_context_data(**kwargs)
+    #    context['now'] = timezone.now()
+    #    return context
