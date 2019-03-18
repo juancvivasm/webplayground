0b63e85d86dbf71fede5e4bcf58c9570963aa75b
Juan Carlos <juanc.vivasm@gmail.com>
2019-03-18 12:59:01 -0400

    Vistas Basada en Clases (CBV - TemplateView)

--- core/views.py
+++ core/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
+# from articles.models import Article
 
-def home(request):
-    return render(request, "core/home.html")
+class HomePageView(TemplateView):
+    template_name = "core/home.html"
 
-def sample(request):
-    return render(request, "core/sample.html")
+    # def get_context_data(self, **kwargs):
+    #    context = super().get_context_data(**kwargs)
+        # context['latest_articles'] = Article.objects.all()[:5]
+    #    context['title'] = 'Mi super Web PlayGround'
+    #    return context
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name, {'title':'Mi super Web PlayGround [@JC]'})
+
+
+class SamplePageView(TemplateView):
+    template_name = "core/sample.html"
+    
