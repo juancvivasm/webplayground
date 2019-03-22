e42e016a4499b48a02490fa6f7aa094b5ea07cc8
Juan Carlos <juanc.vivasm@gmail.com>
2019-03-22 19:51:34 -0400

    App Registration Frm Registro CBV 1.1 (mensaje de registro)

--- registration/views.py
+++ registration/views.py
@@ -5,5 +5,9 @@ from django.urls import reverse_lazy
 # Create your views here.
 class SignUpView(CreateView):
     form_class = UserCreationForm
-    success_url = reverse_lazy('login')
+    # No permite concatenar
+    # success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
+
+    def get_success_url(self):
+        return reverse_lazy('login') + '?register'
