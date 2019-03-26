54745980f7cb6afd15462f5268e7ccf7ab8c1295
Juan Carlos <juanc.vivasm@gmail.com>
2019-03-26 17:38:21 -0400

    App Messenger Segundas pruebas para validar intruso en el hilo
    TDD Refactorizacion de Models de la App Messenger

--- messenger/models.py
+++ messenger/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import m2m_changed
 
 # Create your models here.
 class Message(models.Model):
@@ -13,3 +14,27 @@ class Message(models.Model):
 class Thread(models.Model):
     users = models.ManyToManyField(User, related_name='threads')
     messages = models.ManyToManyField(Message)
+
+def messages_changed(sender, **kwargs):
+    instance = kwargs.pop('instance', None)
+    action = kwargs.pop('action', None)
+    pk_set = kwargs.pop('pk_set', None)
+    print(instance, action, pk_set)
+
+    #Variable para almacenar usuarios y mensajes que no estan en el hilo
+    false_pk_set = set()
+    if action is 'pre_add':
+        for msg_pk in pk_set:
+            msg = Message.objects.get(pk=msg_pk)
+            if msg.user not in instance.users.all():
+                print("Ups, ({}) no forma parte del hilo".format(msg.user))
+                false_pk_set.add(msg_pk)
+            
+        
+        # Forzar la actualización haciendo save (Clave!!! Para que funcione...)
+        instance.save()
+
+    # Se busca los mensajes de false_pk_set que si estan en pk_set y se eliminan
+    pk_set.difference_update(false_pk_set)
+
+m2m_changed.connect(messages_changed, sender=Thread.messages.through)
