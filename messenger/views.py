8cf4674ffc89d1bb2b26ec2ba94df9636594a28c
Juan Carlos <juanc.vivasm@gmail.com>
2019-03-27 16:36:51 -0400

    Urls, Vitas y Templates (1era Consulta)
    Los hilos del usuario autenticado

--- messenger/views.py
+++ messenger/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from .models import Thread
+
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
 # Create your views here.
+@method_decorator(login_required, name="dispatch")
+class ThreadList(ListView):
+    model = Thread
+
+    # Se sobreescribe el metodo para devolver el hilo del usuario autenticado
+    def get_queryset(self):
+        queryset = super(ThreadList, self).get_queryset()
+        return queryset.filter(users=self.request.user)
+
+@method_decorator(login_required, name="dispatch")
+class ThreadDetail(DetailView):
+    model = Thread
